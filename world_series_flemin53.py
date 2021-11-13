920d2d31591a2b0ff8002087fd6b0b274acdc1ca
KahootKing132 <josephflemingvi@gmail.com>
2021-11-13 17:31:30 -0500

    .

--- world_series_flemin53.py
+++ world_series_flemin53.py
@@ -58,12 +58,12 @@ def main():
     totalWins, winners = load_winners_data()
     year = int(input('Enter a year in the range 1903 -- 2020: '))
     if year <= 1903 or year >= 2020:
-        print(f"  Data for the year {year} is not included in this system")
+        print(f"  Data for the year {year} is not included in this system.")
     elif winners[year] != "Not Played":
-        print(f'  The {winners[year]} won the World Series in {year}')
+        print(f'  The {winners[year]} won the World Series in {year}.')
         print(f'  They have won the World Series {totalWins[winners[year]]} times.')
-    elif winners[year] == "Not Played":
-        print(f"  The World Series wasn't played in {year}")
+    else:
+        print(f"  The World Series wasn't played in {year}.")
 
 
 if __name__ == '__main__':
